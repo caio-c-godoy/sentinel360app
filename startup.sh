246e13e981a613ac27fd5673cfcbe86544005fa3
kiocamdoy <caio@4uit.us>
2025-07-22 14:52:13 -0500

    corrige script de startup

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Iniciando aplicação Flask com Gunicorn..."
-/tmp/_preCompressedDestinationDir/antenv/bin/gunicorn --bind=0.0.0.0 --timeout 600 app:app
+/opt/python/3.11.9/bin/gunicorn --bind=0.0.0.0 --timeout 600 app:app
