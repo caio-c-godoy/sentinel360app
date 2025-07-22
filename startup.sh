e3e32f2f8d23465b370971fa52ed655d25f80828
kiocamdoy <caio@4uit.us>
2025-07-22 15:13:53 -0500

    Atualiza arquivos para correção de ambiente no Azure

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Iniciando aplicação Flask com Gunicorn..."
-/opt/python/3.11.9/bin/gunicorn --bind=0.0.0.0 --timeout 600 app:app
+/home/site/antenv/bin/gunicorn --bind=0.0.0.0 --timeout 600 app:app
