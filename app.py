c855e967381d9e05208bf609b3ad17a988677b5e
kiocamdoy <caio@4uit.us>
2025-07-23 17:19:43 -0500

    Rodando app direto com python para debug

--- app.py
+++ app.py
@@ -1406,7 +1406,5 @@ def delete_parceiro(id):
 
     return redirect(url_for("parceiros"))
 
-if (__name__ == "__main__"):
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gunicorn -w 4 -b 0.0.0.0:$PORT app:app
-
+python3 app.py
